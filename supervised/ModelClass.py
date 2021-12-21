ad3e160fc3e3f480e2c5caebfcec2a2588190529
Christian Hiebl <w0011@atcremers9.in.tum.de>
2021-12-21 18:09:44 +0100

    Included another U-Net structure, modified test cases from square to  KITTI data dimensions

--- supervised/ModelClass.py
+++ supervised/ModelClass.py
@@ -69,12 +69,87 @@ class UNET(nn.Module):
         # x = self.final_activation(x)
         return x
 
+class UNET_Mod(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.conv1 = self.contract_block(in_channels, 64, 7, 3)
+        self.conv2 = self.contract_block(64, 128, 3, 1)
+        self.conv3 = self.contract_block(128, 256, 3, 1)
+        self.conv4 = self.contract_block(256, 512, 3, 1)
+
+        self.upconv4 = self.expand_block(512, 256, 3, 1)
+        self.upconv3 = self.expand_block(256*2, 128, 3, 1)
+        self.upconv2 = self.expand_block(128*2, 64, 3, 1)
+        self.upconv1 = self.expand_block(64*2, out_channels, 3, 1)
+
+    def forward(self, x):
+
+        # downsampling part
+        conv1 = self.conv1(x)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        conv4 = self.conv4(conv3)
+
+        upconv4 = self.upconv4(conv4)
+
+        if upconv4.shape != conv3.shape:
+                upconv4 = TF.resize(upconv4, size=conv3.shape[2:])
+        upconv3 = self.upconv3(torch.cat([upconv4, conv3], 1))
+
+        if upconv3.shape != conv2.shape:
+                upconv3 = TF.resize(upconv3, size=conv2.shape[2:])
+        upconv2 = self.upconv2(torch.cat([upconv3, conv2], 1))
+
+        if upconv2.shape != conv1.shape:
+                upconv2 = TF.resize(upconv2, size=conv1.shape[2:])
+        upconv1 = self.upconv1(torch.cat([upconv2, conv1], 1))
+
+        if upconv1.shape != x.shape:
+                upconv1 = TF.resize(upconv1, size=x.shape[2:])
+
+        return upconv1
+
+    def contract_block(self, in_channels, out_channels, kernel_size, padding):
+
+        contract = nn.Sequential(
+            torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding),
+            torch.nn.BatchNorm2d(out_channels),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding),
+            torch.nn.BatchNorm2d(out_channels),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=1)
+                                 )
+
+        return contract
+
+    def expand_block(self, in_channels, out_channels, kernel_size, padding):
+
+        expand = nn.Sequential(torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=padding),
+                            torch.nn.BatchNorm2d(out_channels),
+                            torch.nn.ReLU(),
+                            torch.nn.Conv2d(out_channels, out_channels, kernel_size, stride=1, padding=padding),
+                            torch.nn.BatchNorm2d(out_channels),
+                            torch.nn.ReLU(),
+                            torch.nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1) 
+                            )
+        return expand
+
 def test():
-    x = torch.randn((4, 6, 512, 512))
+    x = torch.randn((4, 6, 375, 1242))
     model = UNET(in_channels=6, out_channels=1)
     preds = model(x)
-    assert preds.shape == (4,1,512,512)
+    assert preds.shape == (4,1,375,1242)
+    print(preds.shape)
+
+def test_mod():
+    x = torch.randn((4, 6, 375, 1242))
+    model = UNET_Mod(in_channels=6, out_channels=1)
+    preds = model(x)
+    assert preds.shape == (4,1,375,1242)
     print(preds.shape)
 
 if __name__ == "__main__":
-    test()
+    # test()
+    test_mod()
