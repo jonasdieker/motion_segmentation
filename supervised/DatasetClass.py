4777605fe08e53e1b60d66f6e6f2a3b246f56dce
Jonas Dieker <ge59bob@mytum.de>
2021-12-29 16:32:23 +0100

    add distinction between train/val and test data for proper testing

--- supervised/DatasetClass.py
+++ supervised/DatasetClass.py
@@ -82,10 +82,9 @@ class ExtendedKittiMod(Dataset):
         # self.mask_paths = sorted(list(glob.glob(os.path.join(self.data_root, "masks/**/image_02/*.png"))))
 
         if test == False:
-            wanted_dirs = ["0005", "0013", "0014", "0015", "0018", "0032", "0051", "0056", "0057", "0059", "0060", "0084"]
-            # wanted_dirs = ["0005"]
+            wanted_dirs = ["0005", "0013", "0014", "0015", "0018", "0032", "0051", "0056", "0057", "0059", "0060"]
         else:
-            wanted_dirs = ["sf"]
+            wanted_dirs = ["0084"]
 
         for sequence_num in wanted_dirs:
             self.image_paths.extend(sorted(list(glob.glob(os.path.join(self.data_root, f"images/2011_09_26_drive_{sequence_num}_sync/data/*.png")))))
