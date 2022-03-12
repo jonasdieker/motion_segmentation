1a378c01f41ba1a4778944d6e7f271d76aa062ac
Christian Hiebl <christian.hiebl@tum.de>
2022-03-12 16:56:35 +0100

    Abstraction with utils, use abstraction in write_static_flow

---
+++ unsupervised/utils_data.py
@@ -0,0 +1,178 @@
+import numpy as np
+from numpy.matlib import repmat
+import matplotlib.pyplot as plt
+from skimage import io
+import cv2
+
+def rotx(angle):
+    return np.array([
+        [1,0,0,0],
+        [0,np.cos((angle*np.pi)/180), -np.sin((angle*np.pi)/180), 0],
+        [0,np.sin((angle*np.pi)/180), np.cos((angle*np.pi)/180), 0],
+        [0,0,0,1]
+    ])
+
+def roty(angle):
+    return np.array([
+        [np.cos((angle*np.pi)/180),0, np.sin((angle*np.pi)/180),0],
+        [0,1,0,0],
+        [-np.sin((angle*np.pi)/180),0, np.cos((angle*np.pi)/180),0],
+        [0,0,0,1]
+    ])
+
+def rotz(angle):
+    return np.array([
+        [np.cos((angle*np.pi)/180), -np.sin((angle*np.pi)/180),0,0],
+        [np.sin((angle*np.pi)/180), np.cos((angle*np.pi)/180),0,0],
+        [0,0,1,0],
+        [0,0,0,1]
+    ])
+
+def build_se3(rotation, translation):
+    se3 = np.hstack((rotation, np.array([translation]).T))
+    se3 = np.vstack((se3,np.array([0,0,0,1])))
+    return se3
+
+def inverse_se3(se3_mat):
+    R_T = se3_mat[:3, :3].T
+    new_t = -R_T.dot(se3_mat[:3,-1])
+
+    return build_se3(R_T, new_t)
+
+def read_depth(depth_file):
+    depth = io.imread(depth_file)
+    depth = depth[:, :, 0] * 1.0 + depth[:, :, 1] * 256.0 + depth[:, :, 2] * (256.0 * 256)
+    depth = depth * (1/ (256 * 256 * 256 - 1))
+    return depth
+
+def get_intrinsics(fov, image_size_x, image_size_y):
+    f = image_size_x/(2.0 * np.tan(fov * np.pi /360))
+    Cx = image_size_x / 2.0
+    Cy = image_size_y / 2.0
+    return np.array([[f, 0, Cx], [0, f, Cy], [0, 0, 1]])
+
+def reproject(u, depth, image_size_x, image_size_y, K=None):
+    '''
+    get [u,v] pixel coordinates and convert to homogeneous
+    get instrinsics
+    multiply inverse K with the homogeneous points and scale depth
+    '''
+    # unpacking (christmas presents)
+    u_coords, v_coords = u[:,0], u[:,1]
+
+    # get homogeneous coords
+    p = np.array([u_coords, v_coords, np.ones_like(u_coords)])
+
+    # get K
+    if K is None:
+        K = get_intrinsics(72/(2*np.pi), image_size_x, image_size_y)
+
+    # get 3D points
+    p3d = np.dot(np.linalg.inv(K), p) * depth.reshape((-1)) * 1000
+    return p3d.T
+
+def transform_pointcloud(pc, trs):
+    # somehow ensure correct direction of trs
+    pc = np.hstack([pc, np.ones((pc.shape[0],1))])
+    pc_trs = trs@pc.T
+    return pc_trs[:3,:].T
+
+def project(p3d, image_size_x, image_size_y):
+    '''
+    gets intrinsics, projects points into the image plane
+    and normalises the pixels
+    '''   
+    K = get_intrinsics(72/(2*np.pi), image_size_x, image_size_y)
+    unnormalised_pixel_coords = np.dot(K, p3d.T).T
+    pixel_coords = unnormalised_pixel_coords/(unnormalised_pixel_coords[:,2].reshape((-1,1)))
+    return pixel_coords[:,:2]
+
+def get_flow(depth1, trs, K=None):
+    # 2d pixel coordinates
+    image_size_x = depth1.shape[1]
+    image_size_y = depth1.shape[0]
+    pixel_length = image_size_x * image_size_y
+    u_coords = repmat(np.r_[image_size_x-1:-1:-1],
+                        image_size_y, 1).reshape(pixel_length)
+    v_coords = repmat(np.c_[image_size_y-1:-1:-1],
+                        1, image_size_x).reshape(pixel_length)
+
+    u_coords = np.flip(u_coords)
+    v_coords = np.flip(v_coords)
+    u = np.array([u_coords, v_coords]).T # u horizontal (x), v vertical (-y)
+
+    pc1 = reproject(u, depth1, image_size_x, image_size_y, K)
+
+    # (CX_T_1 @ 1_T_Sensor1)^-1 @ C2_trs_C1 @ (CX_T_1 @ 1_T_Sensor2)
+    CX_T_Sensor = (roty(90) @ rotz(90))
+    CX_T_Sensor_inv = inverse_se3(CX_T_Sensor)
+    trs = CX_T_Sensor_inv @ trs @ CX_T_Sensor
+
+    pc1_trs = transform_pointcloud(pc1, trs)
+    u_dash = project(pc1_trs, image_size_x, image_size_y)
+
+    flow = u_dash - u
+    flow = flow.reshape(image_size_y, image_size_x, 2)
+
+    return flow
+
+#Visualizations
+
+def plot_pair(img1, img2, size = (15,5)):
+    plt.figure(figsize=size)
+    plt.imshow(np.hstack((img1, img2)))
+    plt.show()
+
+def vis_flow(flow):
+    # create HSV & make Value a constant
+    hsv = np.zeros((512,1382,3))
+    hsv[:,:,1] = 255
+
+    # Encoding: convert the algorithm's output into Polar coordinate
+    flow_reshaped = flow.reshape((512,1382,-1))
+    mag, ang = cv2.cartToPolar(flow_reshaped[...,0], flow_reshaped[...,1])
+    # Use Hue and Value to encode the Optical Flow
+    hsv[:,:, 0] = ang * 180 / np.pi / 2
+    hsv[:,:, 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+    # Convert HSV image into BGR for demo
+    bgr = cv2.cvtColor(hsv.astype(np.uint8), cv2.COLOR_HSV2BGR)
+    plt.figure(figsize=(20,10))
+    plt.imshow(bgr)
+
+def render_optical_flow_data(data):
+    intensity = np.linalg.norm(data, axis=2)
+    angle = np.arctan2(data[:, :, 0], data[:, :, 1])
+    max_intensity = 100
+    # N.B.: an intensity of exactly 1.0 makes the output black (perhaps showing the over-saturation), so keep it < 1
+    intensity = np.clip(intensity, 0, max_intensity - 1) / max_intensity
+    # log scaling
+    basis = 30
+    intensity = np.log1p((basis - 1) * intensity) / np.log1p(basis - 1)
+    # for the angle they use 360° scale, see https://stackoverflow.com/a/57203497/14467327
+    angle = (np.pi + angle) * 360 / (2 * np.pi)
+    # print(F"Ranges, angle: [{np.min(angle)}, {np.max(angle)}], "
+    #       F"intensity: [{np.min(intensity)}, {np.max(intensity)}]")
+    intensity = intensity[:, :, np.newaxis]
+    angle = angle[:, :, np.newaxis]
+    hsv_img = np.concatenate((angle, np.ones_like(intensity), intensity), axis=2)
+    img_out = np.array(cv2.cvtColor(np.array(hsv_img, dtype=np.float32), cv2.COLOR_HSV2RGB) * 256,
+                       dtype=np.dtype("uint8"))
+    plt.figure(figsize=(20,10))
+    plt.imshow(img_out)
+
+def plot_sparse_vecs(flow, sparseness=30, cutoff=1):
+    #Visualization of flow vectors 
+    
+    fig = plt.figure(figsize = (16,10))
+    plt.xlim(0,flow.shape[1])
+    plt.ylim(0,flow.shape[0])
+    plt.gca().invert_yaxis()
+
+    disc_step = sparseness
+    of_offset = 10
+
+    for i in range(of_offset, flow.shape[0]-of_offset, disc_step):
+        for j in range(of_offset,flow.shape[1]-of_offset, disc_step):
+            if np.linalg.norm(flow[i,j,:]) > cutoff:
+                plt.arrow(j,i,flow[i,j,0], flow[i,j,1], head_width=5)
+    fig.show()
---
+++ unsupervised/utils_train.py
@@ -0,0 +1,120 @@
+import sys
+import os
+import gc
+from datetime import datetime
+import time
+import logging
+
+import numpy as np
+from numpy.matlib import repmat
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torchvision.ops.focal_loss import sigmoid_focal_loss
+from torch.utils.data import DataLoader
+
+
+
+#LOGGING
+
+def setup_logger(args, log_root, now_string):
+    formatter = logging.Formatter('[%(levelname)s] %(message)s')
+    logging.basicConfig(
+    format="[%(levelname)s] %(message)s",
+    level=logging.INFO,
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(os.path.join(log_root, f'{now_string}.log'))
+    ])
+    logger = logging.getLogger()
+
+    args.log_root = log_root
+    args.formatter = formatter
+    args.now_string = now_string
+    return args, logger
+
+def refresh_logger(args, logger):
+    # Logging fix for stale file handler
+    logger.removeHandler(logger.handlers[1])
+    fh = logging.FileHandler(os.path.join(args.log_root, f'{args.now_string}.log'))
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(args.formatter)
+    logger.addHandler(fh)
+    return logger
+
+#DATA LOADING
+
+def get_dataloaders(dataset, batch_size=2, dataset_fraction=1.0, shuffle=True):
+    dataset_length = int(len(dataset) * dataset_fraction)
+    train_size = int(0.6 *  dataset_length)
+    val_size = int(0.5*(dataset_length - train_size))
+    test_size = dataset_length - train_size - val_size
+    # taking subset of dataset according to dataset_fraction
+    dataset_idx = list(range(0, dataset_length))
+    dataset = torch.utils.data.Subset(dataset, dataset_idx)
+    train_set, val_set, test_set = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], generator = torch.Generator().manual_seed(0))
+    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=shuffle)
+    val_loader = DataLoader(dataset=val_set, batch_size=batch_size, shuffle=shuffle)
+    test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=shuffle)
+    return train_loader, val_loader, test_loader
+
+def load_model(model_path, device='cuda:0'):
+    if device == "cuda:0":
+        # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        if torch.cuda.is_available():
+            device = torch.device(device)
+            print("Moving model to GPU")
+            return torch.load(model_path).to(device)
+        else:
+            print("No GPU available!")
+
+    if device == "cpu":
+        print("Model on CPU")
+        return torch.load(model_path, map_location=lambda storage, loc: storage)
+
+# Validation
+
+def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor):
+    SMOOTH = 1e-6
+    outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
+    labels = labels.squeeze(1)
+    
+    intersection = torch.sum(torch.bitwise_and(outputs, labels).float(), (1,2))  # Will be zero if Truth=0 or Prediction=0
+    union = torch.sum(torch.bitwise_or(outputs, labels).float(), (1,2))          # Will be zero if both are 0
+    
+    IoU = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
+    aIoU = IoU.mean()
+        
+    return aIoU
+
+
+#Losses
+
+
+def _get_pixel_coords(img1):
+    # 2d pixel coordinates
+    image_size_x = img1.shape[1]
+    image_size_y = img1.shape[0]
+    pixel_length = image_size_x * image_size_y
+    u_coords = repmat(np.r_[image_size_x-1:-1:-1],
+                        image_size_y, 1).reshape(pixel_length)
+    v_coords = repmat(np.c_[image_size_y-1:-1:-1],
+                        1, image_size_x).reshape(pixel_length)
+
+    u_coords = np.flip(u_coords).reshape(-1)
+    v_coords = np.flip(v_coords).reshape(-1)
+    coords1 = np.stack((u_coords, v_coords), axis=-1)
+    return coords1
+
+
+def get_opt_flow_mask(static_flow, dynamic_flow, l_C = 10):
+    """
+    static_flow.shape: (512, 1382, 2)
+    """
+    combo_mask = np.linalg.norm(np.sum((dynamic_flow, -static_flow), axis=0), axis=2)
+    opt_flow_mask = combo_mask < l_C
+
+    return opt_flow_mask
