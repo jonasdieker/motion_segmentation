3c0a1542af5a6c6d76939b5efc28ba838cc02561
Jonas Dieker <ge59bob@mytum.de>
2022-03-13 19:03:26 +0100

    add new unsupervised losses

--- unsupervised/train.py
+++ unsupervised/train.py
@@ -21,134 +21,100 @@ import PIL
 import logging
 import argparse
 
-from utils_data import get_flow, reproject
+from utils_data import get_flow, reproject, get_intrinsics
 from utils_train import setup_logger, refresh_logger, get_dataloaders, iou_pytorch, _get_pixel_coords
 
 
-def get_photometric_error(flow, img1, img2):
+def get_photometric_error(flow, coords2, img1, img2):
+    pixel2 = img2[coords2[0], coords2[1]]
+    coords1 = coords2 + flow[coords2[0], coords2[1]]
+    if coords1[0] < img1.shape[0] and coords1[0] >= 0 and coords1[1] < img1.shape[1] and coords1[1] >=0:
+        pixel1 = img1[coords1[0], coords1[1]]
+    else:
+        pixel1 = pixel2 # value not known
 
-    coords1 = _get_pixel_coords(img1)
-    image_size_x = img1.shape[1]
-    image_size_y = img1.shape[0]
+    p_e = np.sum(np.linalg.norm(pixel2 - pixel1))
+    return p_e
 
-    # reshape images
-    I1 = img1.reshape((-1,3))
-    I2 = img2.reshape((-1,3))
+def get_geometric_error(flow, depth1, depth2, coords2):
+    image_size_x = depth2.shape[1]
+    image_size_y = depth2.shape[0]
+    pixel2_3d = reproject_pixel(coords2, depth2[coords2[0], coords2[1]], image_size_x, image_size_y)
+    coords1 = coords2 + flow[coords2[0], coords2[1]]
+    if coords1[0] < img1.shape[0] and coords1[0] >= 0 and coords1[1] < img1.shape[1] and coords1[1] >=0:
+        pixel1_3d = reproject_pixel(coords1, depth1[coords1[0], coords1[1]], image_size_x, image_size_y)
+    else:
+        pixel1_3d = pixel2_3d
 
-    flow_reshaped = flow.reshape((-1,2))
-    coords2 = (coords1 + flow_reshaped).astype(int)
-    in_image = np.invert((coords2[:,0] < 0) | (coords2[:,0] > image_size_x) | (coords2[:,1] < 0) | (coords2[:,1] > image_size_y))
-    I1_masked = I1[in_image, :]
-    coords2_masked = coords2[in_image, :]
-    I2 = I2[coords2_masked[:,1]*image_size_y+coords2_masked[:,0], :]
-
-    return np.linalg.norm(I1_masked-I2, axis = 1)
-
-
-def get_geometric_error(static_flow, img1, img2, l_geo = 5):
-
-    geometric_mask = 0
-
-    return geometric_mask
+    p_geo = np.sum(np.linalg.norm(pixel2_3d - pixel1_3d))
+    return p_geo
     
-    # get pixel correspondences using flow
-    # check if still in image 2
-    # project pixels in both images to 3D
-    # take l2 norm between 3D points
-    # define some threshold lambda_geo
-    # return sum
+def reproject_pixel(pixel, depth, image_size_x, image_size_y):
+    K = get_intrinsics(72/(2*np.pi), image_size_x, image_size_y)
+    pixel_homogeneous = np.array([pixel[0], pixel[1], 1])
+    p3d = np.dot(np.linalg.inv(K), pixel_homogeneous) * depth * 1000
+    return p3d
 
-
-def consensus_loss(ms_scores, img1, img2, static_flow, dynamic_flow):
+def consensus_loss(ms_scores, img1, img2, depth1, depth2, static_flow, dynamic_flow, l_C):
     """
     ms_preds - motion segmentation prediction
-
     static_pm - static flow photometric error mask
     dynamic_pm - dynamic flow photometric error mask
-
     photometric error, optical flow error, geometric error
-
     summing binary cross entropy over all pixels
-
     """
-    # reshape images
-    I1 = img1.reshape((-1,3))
-    I2 = img2.reshape((-1,3))
-    static_flow_reshaped = static_flow.reshape((-1,2))
-    dynamic_flow_reshaped = dynamic_flow.reshape((-1,2))
+    consensus_loss = 0
 
-    ms_preds = ms_scores.reshape((-1,2))
+    for i in range(len(img1.shape[0])):
+        for j in range(len(im1.shape[1])):
+            #Photometric error (static pe_r, dynamic pe_f)
+            pe_r = get_photometric_error(static_flow, np.array([i,j]), img1, img2)
+            pe_f = get_photometric_error(dynamic_flow, np.array([i,j]), img1, img2)
 
-    pixels = _get_pixel_coords(img1)
+            #Optical flow 
+            flow_diff = np.sum(np.linalg.norm(static_flow[i,j] - dynamic_flow[i,j]))
 
-    for i in range(len(pixels)):
-        #photometric error (static pe_r, dynamic pe_f)
-        pe_r = 0
-        pe_f = 0
-
-        #Optical flow 
-
-        u = dynamic_flow[i]
-
-        #geometric error (static geo_r, dynamic geo_f)
-        geo_r = 0
-        geo_f = 0
+            #geometric error (static geo_r, dynamic geo_f)
+            geo_r = get_geometric_error(static_flow, depth1, depth2, np.array(i,j))
+            geo_f = get_geometric_error(dynamic_flow, depth1, depth2, np.array(i,j))
         
-        #opt
-        # gt_indicator = (pe>0) | opt_flow_e >0 | geo_e > 0
-    #     pixel_loss = nn.functional.binary_cross_entropy(total_mask, ms_scores)
+            label = (pe_r < pe_f) | (flow_diff > l_C) | (geo_r < geo_f)
+            consensus_loss += nn.functional.binary_cross_entropy(ms_scores, label)
 
+    return consensus_loss
 
-    # static_pe_mask = get_photometric_error(static_flow, img1, img2)
+def unsupervised_loss(scores, data, l_M = 1, l_C = 1, l_S = 1):
+    E_M = 0
+    E_C = 0
 
-    # dynamic_pe_mask = get_photometric_error(dynamic_flow, img1, img2)
-    # pe_mask = static_pe_mask < dynamic_pe_mask
+    for i in range(data.shape[0]):
+        img1 = data[i][0][:3,:,:].permute(1,2,0)
+        img2 = data[i][0][3:,:,:].permute(1,2,0)
+        dynamic_flow = data[i][1].permute(1,2,0)
+        static_flow = data[i][2].permute(1,2,0)
+        depth1 = data[i][3][0].permute(1,2,0)
+        depth2 = data[i][3][0].permute(1,2,0)
 
-    # opt_flow_mask = get_opt_flow_mask(static_flow, dynamic_flow)
+        ones = torch.ones(1).expand_as(scores).type_as(scores)
+        E_M += nn.functional.binary_cross_entropy(scores, ones, reduction='sum')
+        E_C += consensus_loss(scores, img1, img2, depth1, depth2, static_flow, dynamic_flow, l_C)
 
-    # geometric_mask = get_geometric_error(static_flow, img1, img2)
-
-    # total_mask = pe_mask | opt_flow_mask | geometric_mask
-
-    # consensus_loss = nn.functional.binary_cross_entropy(total_mask, ms_scores)
-
-    # return consensus_loss
-    return 0
-
-
-def unsupervised_loss(scores, data, l_M = 1, l_E = 1, l_S = 1):
-    # E = lambda_M * E_M + lambda_C * E_C + lambda_S * E_S
-
-    dynamic_flow = data[1] #512x1382x2
-    static_flow = data[2] #512x1382x2
-    img1 = data[0][:3,:,:].permute(1,2,0) #3x512x1382 -> 512x1382x3
-    img2 = data[0][3:,:,:].permute(1,2,0)
-
-    ones = torch.ones(1).expand_as(scores).type_as(scores)
-    E_M = nn.functional.binary_cross_entropy(scores, ones)
-
-    E_C = consensus_loss(scores, img1, img2, static_flow, dynamic_flow)
-
-    total_loss = l_M*E_M + l_M*E_C #+ l_S*E_S
-
+    total_loss = l_M*E_M + l_C*E_C #+ l_S*E_S
     return total_loss
 
-
-def run_val(val_loader, model, epoch, train_size):
+def run_val(val_loader, model, epoch, args):
     model.eval()
     val_losses = []
     val_iou = []
     # needed for validation metrics
     sigmoid = nn.Sigmoid()
     with torch.no_grad():
-        for batch_idx, (data, targets) in enumerate(val_loader):
+        for batch_idx, data in enumerate(val_loader):
             data = data.to(device=device).float()
-            targets = targets.to(device=device).float()
+            motion_seg = data[:,4]
 
             # forward
-            scores = model(data)
-            # loss = sigmoid_focal_loss(scores, targets, alpha=alpha, gamma=gamma, reduction="sum")
-            # loss = criterion(scores, y)
+            scores = model(data[:,0])
             loss = unsupervised_loss(scores, data)
 
             val_losses.append(loss.item())
@@ -156,21 +122,21 @@ def run_val(val_loader, model, epoch, train_size):
 
             if batch_idx == 0:
                 writer.add_images("visualised_preds", scores_rounded, global_step=epoch+1)
-                writer.add_images("visualised_gts_rgb", data[:,0:3,:,:], global_step=epoch+1)
-                writer.add_images("visualised_gts", targets, global_step=epoch+1)
+                writer.add_images("visualised_gts_rgb", data[:,0,3:6,:,:], global_step=epoch+1)
+                writer.add_images("visualised_gts", motion_seg, global_step=epoch+1)
 
-            iou = iou_pytorch(scores_rounded.int(), targets.int())
+            iou = iou_pytorch(scores_rounded.int(), motion_seg.int())
             val_iou.append(iou)
 
         val_loss = sum(val_losses)/len(val_losses)
-        aIoU = sum(val_iou)/len(val_iou)
+        IoU = sum(val_iou)/len(val_iou)
 
-        writer.add_scalar("val loss", val_loss, epoch*train_size)
-        writer.add_scalar("aIoU", aIoU, epoch*train_size)
+        writer.add_scalar("val loss", val_loss, epoch*args.train_size)
+        writer.add_scalar("IoU", IoU, epoch*args.train_size)
 
     # set back to train ensures layers like dropout, batchnorm are used after eval
     model.train()
-    return (val_loss, aIoU)
+    return (val_loss, IoU)
 
 def train(args, prev_model, logger):
 
@@ -191,7 +157,6 @@ def train(args, prev_model, logger):
 
     # loss and optimizer
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
-    criterion = nn.BCEWithLogitsLoss()
     scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=args.lr_scheduler_factor, patience=args.patience, verbose=True)
 
     # for model saving
@@ -200,7 +165,7 @@ def train(args, prev_model, logger):
     # train network
     print("train network ...")
     train_loss = []
-    val_aIoU = []
+    val_IoU = []
     best_val = 1e8
     best_val_epoch = 1
     total_time = 0.0
@@ -210,7 +175,7 @@ def train(args, prev_model, logger):
         losses = []
         steps_per_epoch = len(train_loader)
 
-        for batch_idx, (imgs, dynamic_flow, static_flow, depths) in enumerate(train_loader):
+        for batch_idx, (imgs, dynamic_flow, static_flow, depths, _) in enumerate(train_loader):
 
             # move data to gpu if available
             imgs = imgs.to(device).float()
@@ -219,10 +184,8 @@ def train(args, prev_model, logger):
             depths = depths.to(device).float()
 
             # forward
-            scores = model(imgs)
-            # loss = sigmoid_focal_loss(scores, targets, alpha=alpha, gamma=gamma, reduction="sum")
-            # loss = criterion(scores, targets)
-            loss = unsupervised_loss(scores, imgs)
+            scores = model(data[:,0])
+            loss = unsupervised_loss(scores, data)
 
             # backward
             optimizer.zero_grad()
@@ -238,15 +201,15 @@ def train(args, prev_model, logger):
                 writer.add_scalar("lr change", optimizer.param_groups[0]['lr'], epoch*steps_per_epoch + batch_idx)
 
         train_loss.append(sum(losses)/len(losses))
-        val_aIoU.append(run_val(val_loader, model, epoch, args.train_size))
+        val_IoU.append(run_val(val_loader, model, epoch, args))
         end = time.time()
         total_time += (end-start)
-        scheduler.step(val_aIoU[epoch][0])
+        scheduler.step(val_IoU[epoch][0])
 
         logger = refresh_logger(args, logger)
 
         # info logging to log
-        logger.info(f"Epoch [{epoch + 1}/{args.epochs}] with lr {optimizer.param_groups[0]['lr']}, train loss: {round(train_loss[-1], 5)}, val loss: {round(val_aIoU[-1][0], 5)}, aIoU: {round(val_aIoU[-1][1].item(), 5)}, ETA: {round(((total_time/(epoch+1))*(args.epochs-epoch-1))/60**2,2)} hrs")
+        logger.info(f"Epoch [{epoch + 1}/{args.epochs}] with lr {optimizer.param_groups[0]['lr']}, train loss: {round(train_loss[-1], 5)}, val loss: {round(val_aIoU[-1][0], 5)}, IoU: {round(val_aIoU[-1][1].item(), 5)}, ETA: {round(((total_time/(epoch+1))*(args.epochs-epoch-1))/60**2,2)} hrs")
 
         # check if dir exists, if not create one
         models_root = f"/storage/remote/atcremers40/motion_seg/saved_models/"
@@ -257,16 +220,16 @@ def train(args, prev_model, logger):
             save_path = os.path.join(models_root, f"{model_name_prefix}/{args.batch_size}_{args.lr}_{epoch+1}.pt")
             torch.save(model, save_path)
 
-        #Saving the best aIoU model
-        if val_aIoU[-1][1] < best_val:
-            best_aIoU = val_aIoU[-1][1]
-            best_aIoU_epoch = epoch+1
-            save_path = os.path.join(models_root, f"{model_name_prefix}/best_aIoU.pt")
+        #Saving the best IoU model
+        if val_IoU[-1][1] > best_val:
+            best_val = val_IoU[-1][1]
+            best_IoU_epoch = epoch+1
+            save_path = os.path.join(models_root, f"{model_name_prefix}/best_IoU.pt")
             if os.path.exists(save_path):
                 os.remove(save_path)
             torch.save(model, save_path)
         if (epoch+1) % 10 == 0:
-            logger.info(f"Epoch [{epoch + 1}] Current best aIoU at epoch {best_aIoU_epoch}")
+            logger.info(f"Epoch [{epoch + 1}] Current best IoU at epoch {best_IoU_epoch}")
 
     writer.close()
     # save final model
@@ -283,6 +246,9 @@ def parse():
     parser.add_argument("--lr_scheduler_factor", default=0.5, type=float, help="Learning rate multiplier - default: 3")
     parser.add_argument("--alpha", default=0.25, type=float, help='Focal loss alpha - default: 0.25')
     parser.add_argument("--gamma", default=2.0, type=float, help='Focal loss gamma - default: 2')
+    parser.add_argument("--l_M", default=1.0, type=float, help="hyper-param for motion seg loss")
+    parser.add_argument("--l_C", default=1.0, type=float, help="hyper-param for consensus loss")
+    parser.add_argument("--l_S", default=1.0, type=float, help="hyper-param for regularization")
     parser.add_argument("--load_chkpt", '-chkpt', default='0', type=str, help="Loading entire checkpoint path for inference/continue training")
     parser.add_argument("--dataset_fraction", default=1.0, type=float, help="fraction of dataset to be used")
     return parser
@@ -324,7 +290,7 @@ if __name__ == "__main__":
     data_transforms = transforms.Compose([
         transforms.ToTensor()
     ])
-    dataset = CarlaUnsupervised(data_root)
+    dataset = CarlaUnsupervised(data_root, test=True) # test kwarg needed for plotting ground truth in tensorboard
 
     train_loader, val_loader, test_loader = get_dataloaders(dataset, args.batch_size, args.dataset_fraction)
 
