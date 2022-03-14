1a6db2adfdf02679fa99433ffd63276dc3543397
Christian Hiebl <w0011@atcremers9.in.tum.de>
2022-03-14 10:58:27 +0100

    Refactor supervised train
     - Using utils_train function
    
     - Removing unused imports
    
     - Using args variables directly

--- supervised/train.py
+++ supervised/train.py
@@ -1,52 +1,27 @@
-import torch
-import torchvision
-import torch.nn as nn
-import torch.optim as optim
-import torchvision.transforms as transforms
-from torchvision.ops.focal_loss import sigmoid_focal_loss
-from torch.utils.data import DataLoader
-from DatasetClass import KITTI_MOD_FIXED, ExtendedKittiMod, CarlaMotionSeg
-from ModelClass import UNET, UNET_Mod
-from torch.utils.tensorboard import SummaryWriter
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-import sys
 import os
 import gc
-import matplotlib.pyplot as plt
-import numpy as np
 from datetime import datetime
 import time
-import matplotlib.pyplot as plt
-import PIL
-import logging
 import argparse
 
-def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor):
-    SMOOTH = 1e-6
-    outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
-    labels = labels.squeeze(1)
-    
-    intersection = torch.sum(torch.bitwise_and(outputs, labels).float(), (1,2))  # Will be zero if Truth=0 or Prediction=0
-    union = torch.sum(torch.bitwise_or(outputs, labels).float(), (1,2))          # Will be zero if both are 0
-    
-    IoU = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
-    aIoU = IoU.mean()
-        
-    return aIoU
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision.ops.focal_loss import sigmoid_focal_loss
+from torch.utils.tensorboard import SummaryWriter
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-def refresh_logger(logger):
-    # Logging fix for stale file handler
-    logger.removeHandler(logger.handlers[1])
-    fh = logging.FileHandler(os.path.join(log_root, f'{now_string}.log'))
-    fh.setLevel(logging.INFO)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
-    return logger
+from DatasetClass import KITTI_MOD_FIXED, ExtendedKittiMod, CarlaMotionSeg
+from ModelClass import UNET, UNET_Mod
+from utils_train import setup_logger, refresh_logger, get_dataloaders, iou_pytorch
 
-def run_val(val_loader, model, epoch, train_size):
+def run_val(val_loader, model, epoch, args):
     model.eval()
     val_losses = []
     val_iou = []
+    # needed for validation metrics
+    sigmoid = nn.Sigmoid()
+    criterion = nn.BCEWithLogitsLoss()
     with torch.no_grad():
         for batch_idx, (data, targets) in enumerate(val_loader):
             data = data.to(device=device).float()
@@ -54,8 +29,8 @@ def run_val(val_loader, model, epoch, train_size):
 
             # forward
             scores = model(data)
-            if loss_type == 'focal':
-                loss = sigmoid_focal_loss(scores, targets, alpha=alpha, gamma=gamma, reduction="sum")
+            if args.loss_type == 'focal':
+                loss = sigmoid_focal_loss(scores, targets, alpha=args.alpha, gamma=args.gamma, reduction="sum")
             else:
                 loss = criterion(scores, targets)
 
@@ -73,46 +48,43 @@ def run_val(val_loader, model, epoch, train_size):
         val_loss = sum(val_losses)/len(val_losses)
         IoU = sum(val_iou)/len(val_iou)
 
-        writer.add_scalar("val loss", val_loss, epoch*train_size)
-        writer.add_scalar("IoU", IoU, epoch*train_size)
+        writer.add_scalar("val loss", val_loss, epoch*args.train_size)
+        writer.add_scalar("IoU", IoU, epoch*args.train_size)
 
     # set back to train ensures layers like dropout, batchnorm are used after eval
     model.train()
     return (val_loss, IoU)
 
-def train(lr, batch_size, epochs, patience, loss_type, lr_scheduler_factor, alpha, gamma, prev_model, logger):
+def train(args, prev_model, logger):
 
     # init model and pass to `device`
     input_channels=6
     output_channels=1
-
     if prev_model:
         model = torch.load(prev_model).to(device)
         model = model.float()
-
     else:
         model = UNET(in_channels=input_channels, out_channels=output_channels).to(device)
         # model = UNET_Mod(input_channels, output_channels).to(device)
         model = model.float()
-
     logger.info(f"loaded model of type: {type(model)}")
 
     # loss and optimizer
-    optimizer = optim.Adam(model.parameters(), lr=lr)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
     criterion = nn.BCEWithLogitsLoss()
-    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=lr_scheduler_factor, patience=patience, verbose=True)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=args.lr_scheduler_factor, patience=args.patience, verbose=True)
 
     # for model saving
-    model_name_prefix = now.strftime(f"%d-%m-%Y_%H-%M_bs{batch_size}")
+    model_name_prefix = now.strftime(f"%d-%m-%Y_%H-%M_bs{args.batch_size}")
 
     # train network
     print("train network ...")
     train_loss = []
     val_IoU = []
     best_val = 0
-    best_val_epoch = 1
+    best_IoU_epoch = 1
     total_time = 0.0
-    for epoch in range(epochs):
+    for epoch in range(args.epochs):
         start = time.time()
         model.train()
         losses = []
@@ -126,8 +98,8 @@ def train(lr, batch_size, epochs, patience, loss_type, lr_scheduler_factor, alph
 
             # forward
             scores = model(data)
-            if loss_type == 'focal':
-                loss = sigmoid_focal_loss(scores, targets, alpha=alpha, gamma=gamma, reduction="sum")
+            if args.oss_type == 'focal':
+                loss = sigmoid_focal_loss(scores, targets, alpha=args.alpha, gamma=args.gamma, reduction="sum")
             else:
                 loss = criterion(scores, targets)
 
@@ -145,7 +117,7 @@ def train(lr, batch_size, epochs, patience, loss_type, lr_scheduler_factor, alph
                 writer.add_scalar("lr change", optimizer.param_groups[0]['lr'], epoch*steps_per_epoch + batch_idx)
 
         train_loss.append(sum(losses)/len(losses))
-        val_IoU.append(run_val(val_loader, model, epoch, train_size))
+        val_IoU.append(run_val(val_loader, model, epoch, args.train_size))
         end = time.time()
         total_time += (end-start)
         scheduler.step(val_IoU[epoch][0])
@@ -153,7 +125,7 @@ def train(lr, batch_size, epochs, patience, loss_type, lr_scheduler_factor, alph
         logger = refresh_logger(logger)
 
         # info logging to log
-        logger.info(f"Epoch [{epoch + 1}/{epochs}] with lr {optimizer.param_groups[0]['lr']}, train loss: {round(train_loss[-1], 5)}, val loss: {round(val_IoU[-1][0], 5)}, IoU: {round(val_IoU[-1][1].item(), 5)}, ETA: {round(((total_time/(epoch+1))*(epochs-epoch-1))/60**2,2)} hrs")
+        logger.info(f"Epoch [{epoch + 1}/{args.epochs}] with lr {optimizer.param_groups[0]['lr']}, train loss: {round(train_loss[-1], 5)}, val loss: {round(val_IoU[-1][0], 5)}, IoU: {round(val_IoU[-1][1].item(), 5)}, ETA: {round(((total_time/(epoch+1))*(args.epochs-epoch-1))/60**2,2)} hrs")
 
         # check if dir exists, if not create one
         models_root = f"/storage/remote/atcremers40/motion_seg/saved_models/"
@@ -161,7 +133,7 @@ def train(lr, batch_size, epochs, patience, loss_type, lr_scheduler_factor, alph
             os.mkdir(os.path.join(models_root, model_name_prefix), mode=0o770)
         if (epoch+1) % 5 == 0:
             # save interim model
-            save_path = os.path.join(models_root, f"{model_name_prefix}/{batch_size}_{lr}_{epoch+1}.pt")
+            save_path = os.path.join(models_root, f"{model_name_prefix}/{args.batch_size}_{args.lr}_{epoch+1}.pt")
             torch.save(model, save_path)
 
         #Saving the best IoU model
@@ -177,7 +149,7 @@ def train(lr, batch_size, epochs, patience, loss_type, lr_scheduler_factor, alph
 
     writer.close()
     # save final model
-    save_path = os.path.join(models_root, model_name_prefix, f"{batch_size}_{lr}_{epochs}.pt")
+    save_path = os.path.join(models_root, model_name_prefix, f"{args.batch_size}_{args.lr}_{args.epochs}.pt")
     torch.save(model, save_path)
 
 
@@ -198,31 +170,18 @@ def parse():
 if __name__ == "__main__":
     args = parse().parse_args()
 
-    lr = args.lr
-    batch_size = args.batch_size
-    epochs = args.epochs
-    loss_type = args.loss_type
-    patience = args.patience
-    lr_scheduler_factor = args.lr_scheduler_factor
-    alpha = args.alpha
-    gamma = args.gamma
-    dataset_fraction = args.dataset_fraction
+    root = "/storage/remote/atcremers40/motion_seg/"
 
-    # setup time/date for logging/saving models
+    # data_root = os.path.join(root, "datasets/KITTI_MOD_fixed/training/")
+    # data_root = os.path.join(root, "datasets/Extended_MOD_Masks/")
+    data_root = os.path.join(root, "datasets/Carla_Annotation/Carla_Export/")
+    log_root = os.path.join(root, "logs/")
+    root_tb = os.path.join(root, "runs/")
+
     now = datetime.now()
-    now_string = now.strftime(f"%d-%m-%Y_%H-%M_{batch_size}_{lr}_{epochs}")
-
+    now_string = now.strftime(f"%d-%m-%Y_%H-%M_{args.batch_size}_{args.lr}_{args.epochs}")
     # setup logging
-    log_root = "/storage/remote/atcremers40/motion_seg/logs"
-    formatter = logging.Formatter('[%(levelname)s] %(message)s')
-    logging.basicConfig(
-    format="[%(levelname)s] %(message)s",
-    level=logging.INFO,
-    handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.FileHandler(os.path.join(log_root, f'{now_string}.log'))
-    ])
-    logger = logging.getLogger()
+    args, logger = setup_logger(args, log_root, now_string)
 
     # load checkpoint if path exists
     if os.path.exists(args.load_chkpt):
@@ -234,8 +193,7 @@ if __name__ == "__main__":
         prev_model=None
         logger.warning("Path specified incorrectly, training without a checkpoint model")
 
-    # specify some hyperparams
-    logger.info(f"running with lr={lr}, batch_size={batch_size}, epochs={epochs}, loss_type = {loss_type}, patience={patience}, lr_scheduler_factor={lr_scheduler_factor} alpha={alpha}, gamma={gamma}")
+    logger.info(f"running with lr={args.lr}, batch_size={args.batch_size}, epochs={args.epochs}, loss_type = {args.loss_type}, patience={args.patience}, lr_scheduler_factor={args.lr_scheduler_factor} alpha={args.alpha}, gamma={args.gamma}")
     
     # set device and clean up
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -244,33 +202,12 @@ if __name__ == "__main__":
     torch.cuda.empty_cache()
     logger.info(f"running on '{device}'")
 
-    # dataset
-    # data_root = '/storage/remote/atcremers40/motion_seg/datasets/KITTI_MOD_fixed/training/'
-    # data_root = "/storage/remote/atcremers40/motion_seg/datasets/Extended_MOD_Masks/"
-    data_root = "/storage/remote/atcremers40/motion_seg/datasets/Carla_Annotation/Carla_Export/"
-    data_transforms = transforms.Compose([
-        transforms.ToTensor()
-    ])
     # dataset = ExtendedKittiMod(data_root)
     dataset = CarlaMotionSeg(data_root)
 
-    # initialise tensorboard
-    writer = SummaryWriter("/storage/remote/atcremers40/motion_seg/runs/" + now_string)
+    train_loader, val_loader, test_loader = get_dataloaders(dataset, args.batch_size, args.dataset_fraction)
 
-    # needed for validation metrics
-    sigmoid = nn.Sigmoid()
+    # initialize tensorboard
+    writer = SummaryWriter(root_tb + now_string)
 
-    # data split and data loader
-    dataset_length = int(len(dataset) * dataset_fraction)
-    train_size = int(0.6 *  dataset_length)
-    val_size = int(0.5*(dataset_length - train_size))
-    test_size = dataset_length - train_size - val_size
-    # taking subset of dataset according to dataset_fraction
-    dataset_idx = list(range(0, dataset_length))
-    dataset = torch.utils.data.Subset(dataset, dataset_idx)
-    train_set, val_set, test_set = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], generator = torch.Generator().manual_seed(0))
-    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(dataset=val_set, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
-
-    train(lr, batch_size, epochs, patience, loss_type, lr_scheduler_factor, alpha, gamma, prev_model, logger)
+    train(args, prev_model, logger)
